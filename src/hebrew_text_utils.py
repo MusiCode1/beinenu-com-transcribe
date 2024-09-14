3b9f32992953c0571d2346b1c2e5f7df1fd210dc
MusiCode1 <54508123+MusiCode1@users.noreply.github.com>
2024-09-14 15:34:21 +0300

    Update hebrew_text_utils.py

--- src/hebrew_text_utils.py
+++ src/hebrew_text_utils.py
@@ -12,7 +12,7 @@ def remove_hebrew_niqqud(text: str):
 
 def remove_pesukim_letters(text: str):
 
-    psukim_pattern = re.compile(r"(\s|^)[א-ת]{1,2}\s")
+    psukim_pattern = re.compile(r"(^|\.\s|\s\s)[א-ת]{1,2}\s")
 
     clean_text = re.sub(psukim_pattern, "\r\n", text)
 
@@ -35,3 +35,12 @@ def replace_colon_and_semicolon_with_comma(text: str):
 
 def remove_extra_characters(text: str, chars:str):
     return text.replace(chars, '')
+
+def clean_the_text(text):
+    clean_text = remove_pesukim_letters(text)
+    clean_text = replace_colon_and_semicolon_with_comma(clean_text)
+    clean_text = remove_curly_braces_and_content(clean_text)
+    clean_text = remove_hebrew_niqqud(clean_text)
+    clean_text = remove_extra_spaces(clean_text)
+    clean_text = clean_text.replace("--", " ")
+    return clean_text
